c95eb5d4db9fdface58c8d22ec31e20d883e02f4
Douglas-Dauto <dautodouglasdauto@gmail.com>
2023-07-28 19:09:57 -0300

    Modificado aula 69

--- aula69/metodoForEachDeArrays.js
+++ aula69/metodoForEachDeArrays.js
@@ -4,6 +4,8 @@
 
 const array = [5, 20, 42, 24, 4, 22, 84, 63, 55];
 
+// A função de callback pode conter 3 parâmetros, valor, indice e o array completo. Os argumentos são passados automaticamente pela função forEach.
+// Podemos omitir os parâmetros da função de callback se não formos utiliza-las.
 array.forEach(function(valor, indice, array) {
     console.log(valor, indice, array); // O código sera repetido igual a estrutura for
 });
